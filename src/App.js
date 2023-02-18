e66eaf521c393e310678dfca9a354e61a2933df7
KARANVEERMANOHAR <74814835+KARANVEERMANOHAR@users.noreply.github.com>
2023-02-18 23:49:10 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import FormBuilder from 'react-form-builder2';
 
 function App() {
+  const [formData, setFormData] = useState([]);
+  const [undoStack, setUndoStack] = useState([]);
+  const [redoStack, setRedoStack] = useState([]);
+  const [previewData, setPreviewData] = useState(null);
+
+  const handleFormChange = (newFormData) => {
+    setUndoStack([...undoStack, formData]);
+    setFormData(newFormData);
+    setRedoStack([]);
+  };
+
+  const handleUndo = () => {
+    if (undoStack.length > 0) {
+      const prevFormData = undoStack.pop();
+      setRedoStack([...redoStack, formData]);
+      setFormData(prevFormData);
+      setUndoStack([...undoStack]);
+    }
+  };
+
+  const handleRedo = () => {
+    if (redoStack.length > 0) {
+      const nextFormData = redoStack.pop();
+      setUndoStack([...undoStack, formData]);
+      setFormData(nextFormData);
+      setRedoStack([...redoStack]);
+    }
+  };
+
+  const handlePreview = () => {
+    const previewData = { fields: formData };
+    setPreviewData(previewData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FormBuilder.ReactFormBuilder formData={formData} onChange={handleFormChange} />
+      <button onClick={handleUndo} disabled={undoStack.length === 0}>
+        Undo
+      </button>
+      <button onClick={handleRedo} disabled={redoStack.length === 0}>
+        Redo
+      </button>
+      <button onClick={handlePreview}>Preview</button>
+      {previewData && (
+        <pre>{JSON.stringify(previewData, null, 2)}</pre>
+      )}
     </div>
   );
 }
